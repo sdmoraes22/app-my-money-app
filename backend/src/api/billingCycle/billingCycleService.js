fc6186cd9490e3713d0020242b08b5a09e8eb8ce
Cristiano Moraes <cmoraes.ti@gmail.com>
2018-09-02 11:53:44 -0300

    Revert "Criação da API REST para o ciclo de pagamentos."
    This reverts commit 76f5ba726003e42b4dffd8a78fee9c152a88ec53.

--- backend/src/api/billingCycle/billingCycleService.js
+++
@@ -1,6 +0,0 @@
-const BillingCycle = required('./billingCycle')
-
-BillingCycle.methods(['get', 'post', 'put', 'delete'])
-BillingCycle.updateOptions({new: true, runValidators: true})
-
-modeule.exports = BillingCycle
