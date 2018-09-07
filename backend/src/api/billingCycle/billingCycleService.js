f1aec5557206c5675e381dc5ff9e00b204156365
cmoraes.ti <cmoraes.ti@hotmail.com>
2018-09-07 14:08:08 -0300

    Atualização para obter a quantidade de registros

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -4,4 +4,14 @@ const BillingCycle = require ('./billingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error) {
+            res.status(500).json({erros: [error]})
+        } else {
+            res.json({value})
+        }
+    })
+})
+
 module.exports = BillingCycle
