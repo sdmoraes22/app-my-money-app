259d9abe5d1d47df4ed891f0b4ac35b0e1eb4006
Cristiano Moraes <cmoraes.ti@gmail.com>
2018-09-21 21:36:09 -0300

    Correções no webpack config e criação dos pimeiros componentes.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entry = './src/index.jsx',
+    entry : './src/index.jsx',
     output: {
         path: __dirname + '/public',
         filename: './app.js'
@@ -12,20 +12,22 @@ module.exports = {
         contentBase: './public',
     },
     resolve: {
-        extensions: ['', 'js', 'jsx'],
+        extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/JQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/plugins/bootstrap/js/bootstrap.js'
-        },
-        plugins: [
-            new webpack.ProvidePlugin({
-                $: 'jquery',
-                jQuery: 'jquery',
-                'window.jQuery': 'jquery'
-            }),
-            new ExtractTextPlugin('app.css')
-        ],
+        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new ExtractTextPlugin('app.css')
+    ],
+    module: {
         loaders: [{
             test: /.js[x]?$/,
             loader: 'babel-loader',
