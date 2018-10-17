1cfb0db93e7b7ff8d0f7924cf7650c88abce142c
Cristiano Moraes <cmoraes.ti@gmail.com>
2018-10-16 22:52:46 -0300

    Crisção do footer para a estrutura da aplicação.

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2018 <a href='#' target='_blank'>Cristiano Moraes</a>.
+        </strong>
+    </footer>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,12 +1,17 @@
 import '../common/template/dependencies'
 import React from 'react'
 
-import Header from "../common/template/header"
-import Sidebar from "../common/template/sidebar"
+import Header from '../common/template/header'
+import Sidebar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
 
 export default props => (
     <div className='wrapper'>
-         <Header />
-         <Sidebar />
+        <Header />
+        <Sidebar />
+        <div className='content-wrapper'>
+            <h1>Conteúdo</h1>
+        </div>
+        <Footer />
     </div>
 )
