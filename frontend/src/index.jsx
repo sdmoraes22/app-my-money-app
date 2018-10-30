8c57d6762b599e89fbb9bc543ec8eaea3ecd461f
Cristiano Moraes <cmoraes.ti@gmail.com>
2018-10-30 10:51:34 -0200

    Integração do Redux DevTools na aplicação.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -8,7 +8,9 @@ import promise from 'redux-promise'
 import App from './main/app'
 import reducers from './main/reducers'
 
-const store = applyMiddleware(promise)(createStore)(reducers)
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+            && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(promise)(createStore)(reducers, devTools)
 
 ReactDom.render(
     <Provider store={store}>
