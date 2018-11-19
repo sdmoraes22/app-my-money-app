f3d8042262bd995334aed42703f5ec82ddd4d430
Cristiano Moraes <cmoraes.ti@gmail.com>
2018-11-18 22:44:21 -0200

    Adicionado campo de status no componente itemList.

--- frontend/src/billingCycle/itemList.jsx
+++ frontend/src/billingCycle/itemList.jsx
@@ -2,9 +2,9 @@ import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import {Field, arrayInsert, arrayRemove} from 'redux-form'
-
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
+import If from '../common/operator/if'
 
 
 
@@ -29,6 +29,10 @@ class ItemList extends Component {
                     placeholder='Informe o nome' readOnly={this.props.readOnly} /></td>
                 <td><Field name={`${this.props.field}[${index}].value`} component={Input}
                     placeholder='Informe o valor' readOnly={this.props.readOnly}/></td>
+                <If test={this.props.showStatus}>
+                    <td><Field name={`${this.props.field}[${index}].status`} component={Input}
+                        placeholder='Informe o status' readOnly={this.props.readOnly}/></td>
+                </If>
                 <td>
                     <button type='button' className='btn btn-success'
                         onClick={() => this.add(index + 1)}>
@@ -58,6 +62,9 @@ class ItemList extends Component {
                             <tr>
                                 <th>Nome</th>
                                 <th>Valor</th>
+                                <If test={this.props.showStatus}>
+                                    <th>Status</th>
+                                </If>
                                 <th className='table-actions'>Ações</th>
                             </tr>
                         </thead>
