557f84051a8154e0e55da26425753dc95f05eae3
Cristiano Moraes <cmoraes.ti@gmail.com>
2018-12-18 23:08:24 -0200

    Atualização de todo o Dashboard para as novas dependências

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,7 +8,7 @@ import promise from 'redux-promise'
 import multi from 'redux-multi'
 import thunk from 'redux-thunk'
 
-import Routes from './main/routes'
+import AuthOrApp from './main/authOrApp'
 import reducers from './main/reducers'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
@@ -17,7 +17,7 @@ const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devT
 
 ReactDom.render(
     <Provider store={store}>
-        <Routes/>
+        <AuthOrApp />
     </Provider>
 , document.getElementById('app'))
 
